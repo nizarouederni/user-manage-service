0ef232806b749fe8ff28f361f427c5bc3a23476f
nizar ouederni <nizarouederni@nizars-macbook-pro.local>
2022-12-28 20:00:31 -0500

    first commit

--- src/main/java/com/myservice/usermanageservice/dto/UserDto.java
+++ src/main/java/com/myservice/usermanageservice/dto/UserDto.java
@@ -1,14 +1,11 @@
 package com.myservice.usermanageservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
     private Long id;
     private String firstName;
