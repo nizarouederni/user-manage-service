53c2bfdc3974ecf2e3ffc3a6e01b5ef116a2653a
nizarouederni <mnazour@gmail.com>
2023-01-05 08:57:06 -0500

    springboot 2.7.6

--- src/main/java/com/myservice/usermanageservice/entity/User.java
+++ src/main/java/com/myservice/usermanageservice/entity/User.java
@@ -1,10 +1,13 @@
 package com.myservice.usermanageservice.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
+
 
 @Setter
 @Getter
